9ec60e693ff4e7b823301f710fc8d1dc16e22fde
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-16 08:12:21 -0300

    fix: Concertando o base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { Home } from './components/home/home';
+import { QuemSomos } from './components/quem-somos/quem-somos';
 
 export const routes: Routes = [
   {
@@ -9,5 +10,9 @@ export const routes: Routes = [
   {
     path: "home",
     component: Home
+  },
+  {
+    path: "quemsomos",
+    component: QuemSomos
   }
 ];
