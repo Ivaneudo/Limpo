29be097d72b1359e7b4037a7134744a4aabce55a
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-16 12:38:12 -0300

    feat: Adicinadno os components ao form

--- src/app/components/form/form.ts
+++ src/app/components/form/form.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Inputs } from "../inputs/inputs";
+import { Button } from "../button/button";
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [Inputs, Button],
   templateUrl: './form.html',
   styleUrl: './form.css',
 })
