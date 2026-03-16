8dd0bc85e522da5884a82572c75b6cfa59acb75e
Ivaneudo <ivaneudo.dev@gmail.com>
2026-03-16 08:11:24 -0300

    feat: craindo a Quem Somos

---
+++ src/app/components/quem-somos/quem-somos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quem-somos',
+  imports: [],
+  templateUrl: './quem-somos.html',
+  styleUrl: './quem-somos.css',
+})
+export class QuemSomos {}
